4df5db9d1c74f719a33da903232f793ac956e3bd
ModernShrub <75245177+ModernShrub@users.noreply.github.com>
2021-02-02 17:39:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+objects = [];
+status = "";
+
+function preload(){
+  video = createVideo('video.mp4');
+}
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video.hide();
+  }
+  
+function start()
+{
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
